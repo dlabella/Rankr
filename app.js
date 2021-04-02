16f0a9895c9566a9b41266f3b1a9c64927aa265e
dlabella <dani.labella@gmail.com>
2021-04-02 16:28:03 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,133 @@
+//Sheet Id
+//https://docs.google.com/spreadsheets/d/1Za8jT8su5c5usc5gZhaKOaIiGs1MGmQtITfBs4aQqd0/edit#gid=0
+//                                       ^------------------------------------------^
+const sheetId = "1Za8jT8su5c5usc5gZhaKOaIiGs1MGmQtITfBs4aQqd0";
+const credentials = require('./client-secrets.json');
+
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const express = require("express");
+const rankingRow = 1;
+var doc = null;
+var app = express();
+app.use(express.json());
+
+
+var ADMIN_signature = "282a2202bb6a31eb327a71c1affc9648";
+
+app.use(express.urlencoded({
+  extended: true
+}));
+var port = process.env.PORT || 3000;
+console.log("Loading Data");
+app.listen(port, () => {
+  console.log("Server running on port " + port);
+});
+
+
+app.use(express.static('content'));
+app.get("/rankrTable", async (req, res, next) => {
+  if (req.query.forced === "true" || doc == null) {
+    await loadDocument();
+  }
+  try {
+    var data = await getSpreadSheedData();
+    res.json(data);
+  } catch (ex) {
+    doc = null;
+    res.sendStatus(500);
+  }
+});
+app.get("/authorize", async (req, res, next) => {
+  if (isAuthorised(req)) {
+    return true;
+  }
+  return false;
+});
+app.get("/datasource", async (req, res, next) => {
+  if (isAuthorised(req)) {
+    return "https://docs.google.com/spreadsheets/d/" + sheetId + "/edit#gid=0";
+  }
+  return "";
+});
+app.post("/match", async (req, res, next) => {
+  if (doc == null) {
+    await loadDocument();
+  }
+  var data = req.body;
+  var historySheet = await doc.sheetsByIndex[1];
+  var row = {
+    "Date": data.matchDate,
+    "Winner": data.winnerTeam,
+    "WinnerELO": data.winnerTeamELO,
+    "WinnerRank": data.winnerTeamRank,
+    "WinnerIncrease": data.winnerTeamIncrease,
+    "WinnnerUpset": data.winnerTeamUpset,
+    "Loser": data.loserTeam,
+    "LoserELO": data.loaseTeamELO,
+    "LoserRank": data.loserTeamRank,
+    "StockDiff": data.stockDiff
+  }
+  var rows = [];
+  rows.push(row);
+
+  try {
+    await historySheet.addRows(rows);
+  } catch (ex) {
+    doc = null;
+    res.sendStatus(500);
+  }
+});
+
+app.post("/crew", async (req, res, next) => {
+  if (doc == null) {
+    await loadDocument();
+  }
+  var data = req.body;
+  var rankSheet = await doc.sheetsByIndex[0];
+  var row = {
+    "#": "_",
+    "Tag": data.crewTag.toUpperCase(),
+    "Crew": data.crewName,
+  }
+  var rows = [];
+  rows.push(row);
+
+  try {
+    await rankSheet.addRows(rows);
+  } catch (ex) {
+    doc = null;
+    res.sendStatus(500);
+  }
+});
+
+function isAuthorised(request) {
+  if (req.query.token === "282a2202bb6a31eb327a71c1affc9648") {
+    return true;
+  }
+  return false;
+}
+
+async function loadDocument() {
+  doc = new GoogleSpreadsheet(sheetId);
+  await doc.useServiceAccountAuth(credentials);
+  await doc.loadInfo();
+}
+
+async function getSpreadSheedData() {
+  var sheet = await doc.sheetsByIndex[0];
+  var rows = await sheet.getRows();
+  var dataSet = {
+    data: []
+  };
+  for (var rowIndex = 0; rowIndex < rows.length; rowIndex++) {
+    var dataRow = [];
+    var row = rows[rowIndex];
+    for (var colIndex = 0; colIndex < row._rawData.length; colIndex++) {
+      dataRow.push(row._rawData[colIndex]);
+    }
+    if (dataRow[0] !== "") {
+      dataSet.data.push(dataRow);
+    }
+  }
+  return dataSet;
+}
---
+++ content/js/rankr.js
@@ -0,0 +1,208 @@
+const tableId = "rankrTable";
+var tableData = {};
+var matchModal = {};
+var crews = [];
+const crewTag = 1;
+const crewELO = 5;
+const crewRank = 0;
+var authToken = "";
+
+$(document).ready(function () {
+    M.AutoInit();
+    var matchModalEl = document.getElementById("match-result");
+    matchModal = M.Modal.getInstance(matchModalEl);
+
+    var crewModalEl = document.getElementById("add-crew");
+    crewModal = M.Modal.getInstance(crewModalEl);
+
+    var dateEl = document.getElementById("matchDate");
+    var dateInstance = M.Datepicker.init(dateEl, { format: 'yyyy-mm-dd', defaultDate: new Date() });
+
+    $(".js-reload").click(function(){
+        loadRankData(true);
+    })
+
+    manageFormSubmit("match-result-form", "match-result-submit", "/match",
+        ()=> { matchModal.close(); },
+        () => { loadRankData(true); });
+
+    manageFormSubmit("add-crew-form", "add-crew-submit", "/crew",
+        ()=> { crewModal.close(); },
+        () => { loadRankData(true); });
+
+    $("input").change(function () {
+        var newELO = computeData();
+        if (newELO) {
+            $("#winnerTeamIncrease").val(newELO);
+        }
+    });
+    matchModal.options.onOpenEnd = function () {
+        dateInstance.setDate(new Date());
+    };
+
+    loadRankData();
+});
+
+function manageFormSubmit(formId, submitId, url, completeCb, successCb) {
+    $("#" + submitId).click(function (e) {
+        e.preventDefault();
+        var form = $("#" + formId);
+        var formData = form.serialize();
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: formData,
+            success: function (data) {
+                if (successCb) {
+                    successCb();
+                }
+            },
+            fail: function (xhr, textStatus, errorThrown) {
+                alert('There was a problem updating the data, check datasource');
+                console.log(errorThrown);
+            }
+        });
+        if (completeCb) {
+            completeCb();
+        }
+    });
+}
+
+function loadRankData(forced) {
+    $("#tableLoading").show();
+    var f = 'false';
+    if (forced===true){
+        f='true';
+    }
+    return fetch('/rankrTable?forced='+f)
+        .then(response => {
+            processResponse(response).then(() => {
+                $("#tableLoading").hide();
+                $('#' + tableId).show();
+
+                initCrewAutocomplete();
+            });
+        })
+        .catch(ex => {
+            console.log("ERROR: " + ex)
+        });
+}
+
+function initCrewAutocomplete() {
+    var crewList = {};
+    for (var row of tableData) {
+        var tag = row[crewTag];
+        crewList[tag] = null;
+    }
+    var winnerAutocomplete = document.getElementById('winnerTeam');
+    var loserAutocomplete = document.getElementById('loserTeam');
+    M.Autocomplete.init(winnerAutocomplete, { data: crewList, onAutocomplete: winnerAutocompleted });
+    M.Autocomplete.init(loserAutocomplete, { data: crewList, onAutocomplete: loserAutocompleted });
+}
+
+function winnerAutocompleted(value) {
+    var elo = getCrewELO(value);
+    $("#winnerTeamELO").val(elo);
+}
+
+function loserAutocompleted(value) {
+    var elo = getCrewELO(value);
+    $("#loaseTeamELO").val(elo);
+}
+
+function getCrewELO(crew) {
+    for (var row of tableData) {
+        if (row[crewTag] === crew) {
+            return row[crewELO];
+        }
+    }
+    return 0;
+}
+function getRankDifference(crewAELO, crewBELO) {
+    var arank = getRankFromELO(crewAELO);
+    var brank = getRankFromELO(crewBELO);
+    var result = arank - brank;
+    if (result < 0) {
+        result = result * -1;
+    }
+    return result;
+}
+
+function getRankFromELO(ELO) {
+    if (ELO <= 1349) {
+        return 1;
+    }
+    if (ELO <= 1499) {
+        return 2;
+    }
+    if (ELO <= 1749) {
+        return 3;
+    }
+    if (ELO <= 1999) {
+        return 4;
+    }
+    if (ELO >= 2000) {
+        return 5;
+    }
+}
+
+function computeData() {
+    var winner = $('#winnerTeam').val();
+    var loser = $('#loserTeam').val();
+    var P = null;
+    var Q = null;
+
+    if (winner) {
+        Q = getCrewELO(winner);
+        var winnerRank = getRankFromELO(Q);
+        $('#winnerTeamRank').val(winnerRank);
+    }
+
+    if (loser) {
+        P = getCrewELO(loser);
+        var loserRank = getRankFromELO(P);
+        $('#loserTeamRank').val(loserRank);
+    }
+
+    var dStr = $("#stockDiff").val();
+    if (!dStr) {
+        return null;
+    }
+    var d = parseInt(dStr);
+    var R = getRankDifference(Q, P);
+    var mode = "-";
+    if (winnerRank >= loserRank) {
+        var result = (P / Q * ((50 + d) * (1 - R / 5)));
+    } else {
+        mode = "Y"
+        var result = (P / Q * ((50 + d) * (1 + R / 5)));
+    }
+    $("#winnerTeamUpset").val(mode);
+    return Math.round(result);
+}
+
+function processResponse(response, table) {
+    return response.json().then(data => {
+        tableData = data.data;
+        $('#' + tableId).DataTable({
+            data: tableData
+        });
+    })
+
+}
+
+$.fn.serializeObject = function () {
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function () {
+        if (o[this.name]) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
